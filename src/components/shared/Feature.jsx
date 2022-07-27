8512fd5f980d3235b12e8f1a4cc96c8adfd3e5c9
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-07-28 00:07:28 +0300

    shared

---
+++ src/components/shared/Feature.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BsPatchCheckFill } from 'react-icons/bs'
+
+const Feature = ({ title, level }) => {
+    return (
+
+        <div className='feature'>
+            <BsPatchCheckFill/>
+            <div className='feature-inner'>
+                <h2>{title}</h2>
+                <p>{level}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Feature 
