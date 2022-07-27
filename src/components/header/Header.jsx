e4e32d9428167a1477d303372f45dfc2b968c52d
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-07-27 23:10:15 +0300

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './header.css'
+import { BsLinkedin, BsGithub, BsFileText } from 'react-icons/bs'
+import { HiChevronDoubleDown } from 'react-icons/hi'
+import { Button } from 'components'
+import {ButtonOutline} from 'components/shared/Button'
+// import ME from 'assets/me.png'
+
+const Header = () => {
+    return (
+        <header className='header' id="home">
+            <div className='header-header'>
+                <div className='header-title'>
+                    <h5>Hello I'm</h5>
+                    <h1>George Gichuru</h1>
+                    <small>FullStack Developer</small>
+                </div>
+                <div className='header-buttons'>
+                    <ButtonOutline text="Download CV"/>
+                    <Button text="Let's Talk"/>
+                </div>
+            </div>
+
+            <div className='header-footer'>
+                <div className='header-socials'>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <BsLinkedin/>
+                    </a>
+
+                    <a href="https://github.com/GEORGE-GICHURU/" target="_blank" rel="noopener noreferrer">
+                        <BsGithub/>
+                    </a>
+
+                    <a href="#!" target="_blank" rel="noopener noreferrer">
+                        <BsFileText/>
+                    </a>
+                </div>
+
+                <div className='image-area'>
+                    <img src="{ME}" alt="George Gichuru" />
+                </div>
+                <a href="#!" className='scroll-down'>
+                    <span>Scroll Down</span>
+                    <HiChevronDoubleDown/>
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
