a9d1e589665c38af21871387dc77edee0afb3b0a
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-08-02 00:19:22 +0300

    several changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
                 </div>
 
                 <div className='image-area'>
-                    <img src="image.png" alt="George Gichuru" />
+                    <img src="{image.png}" alt="George Gichuru" />
                 </div>
                 <a href="#!" className='scroll-down'>
                     <span>Scroll Down</span>
